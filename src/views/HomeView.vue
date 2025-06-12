46073c4f58f34d6340a9127c77a6a739f3a90b07
camismouri123 <camis.mouri@gmail.com>
2025-06-12 20:34:52 -0300

    camila escribe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@
   <main>
     <div class ="d-flex justify-content-center align-items-center vh-100">
       <h1 class = "text-success text-center">Inicio</h1>
+      hola aca cami escribió
     </div>
     
   </main>
